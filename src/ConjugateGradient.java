76734f823e5053b5769e07b8c5e7ff7e09d97915
Natalia Senatorova <st068742@student.spbu.ru>
2019-12-09 19:58:02 +0300

    Update ConjugateGradient.java

--- src/ConjugateGradient.java
+++ src/ConjugateGradient.java
@@ -2,38 +2,30 @@ package ru.spbu.apmath.prog.methods;
 
 class ConjugateGradient {
 
-    Vector conjugateGrad(Func func){
-        double  a = 0;
-        double  b = 10;
-        int i = 1;
+    Vector conjugateGrad(Func func) {
+        double a = 0, b = 10;
         double x1, x2, alpha, beta;
-
+        int i = 1;
         Vector v = new Vector(2, 1);
-        Vector grad = func.grad(v);
-        Vector anti_grad = grad.multiplyV(-1);
-        Vector direction_previous = anti_grad;
-        Vector direction_current;
+        Vector anti_grad = func.grad(v).multiplyV(-1);
+        Vector direct_previous = anti_grad;
+        Vector direct_current;
 
-        double epsilon = 0.0001;
+        double epsilon = 0.0011;
         while (func.grad(v).getNorm() > epsilon) {
             anti_grad = func.grad(v).multiplyV(-1);
-            //ортогонализация Грама-Шмидта
             if (i == 1) {
-                //первый вектор ортогон-ть не нужно
-                direction_current = anti_grad;
+                direct_current = anti_grad;
             } else {
-                //коэффициент для ортогонализации (x,y)/(y,y)
-                beta = (anti_grad.skalmul(anti_grad)) / (direction_previous.skalmul(direction_previous));
-                //найдем направление спуска g = x - beta*y
-                direction_current = anti_grad.substractV(direction_previous.multiplyV(beta));
-                //System.out.println("Новое напрвление: " + direction_current.represent());
+                beta = (anti_grad.skalmul(direct_previous)) / (direct_previous.skalmul(direct_previous));
+                direct_current = anti_grad.substractV(direct_previous.multiplyV(beta));
             }
             do {
                 double middle = (a + b) / 2;
                 x1 = middle - 0.0005;
                 x2 = middle + 0.0005;
-                double i1 = func.val(v.addV(direction_current.multiplyV(x1)));
-                double i2 = func.val(v.addV(direction_current.multiplyV(x2)));
+                double i1 = func.val(v.addV(direct_current.multiplyV(x1)));
+                double i2 = func.val(v.addV(direct_current.multiplyV(x2)));
                 if (i1 < i2) {
                     b = x2;
                 } else {
@@ -41,12 +33,12 @@ class ConjugateGradient {
                 }
             } while (Math.abs(b - a) > epsilon);
             alpha = (a + b) / 2;
-            v = v.addV(direction_current.multiplyV(alpha));
-            direction_previous = direction_current;
-            //System.out.println("k = " + iter + " Точка: " + v.represent() + " Значение: " + ros.getValue(v));
+            v = v.addV(direct_current.multiplyV(alpha));
+            direct_previous = direct_current;
             i++;
 
         }
+        System.out.println(i + " итераций");
         return v;
     }
 }
