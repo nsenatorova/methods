fbead6a9188e59d4482f2d73ac4457c2646edb45
Natalia Senatorova <st068742@student.spbu.ru>
2019-12-09 20:02:04 +0300

    Update HimmelblauFunc.java

--- src/HimmelblauFunc.java
+++ src/HimmelblauFunc.java
@@ -3,20 +3,23 @@ package ru.spbu.apmath.prog.methods;
 public class HimmelblauFunc implements Func {
     public double b;
     public double a;
-    HimmelblauFunc(double a, double b){
+
+    HimmelblauFunc(double a, double b) {
         this.a = a;
         this.b = b;
     }
-    public double val(Vector v){
+
+    public double val(Vector v) {
         double x = v.x;
         double y = v.y;
-        return Math.pow((Math.pow(x,2) + y - a),2) + Math.pow((x + Math.pow(y,2) - b),2);
+        return Math.pow((Math.pow(x, 2) + y - a), 2) + Math.pow((x + Math.pow(y, 2) - b), 2);
     }
-    public Vector grad(Vector v){
+
+    public Vector grad(Vector v) {
         double x = v.x;
         double y = v.y;
-        double dx = 2*(Math.pow(x,2) + y -a)*2*x + 2*(x + Math.pow(y,2) - b);
-        double dy = 2*(Math.pow(x,2) + y -a) + 2*(x + Math.pow(y,2) - b)*2*y;
+        double dx = 2 * (Math.pow(x, 2) + y - a) * 2 * x + 2 * (x + Math.pow(y, 2) - b);
+        double dy = 2 * (Math.pow(x, 2) + y - a) + 2 * (x + Math.pow(y, 2) - b) * 2 * y;
         return new Vector(dx, dy);
     }
 }
