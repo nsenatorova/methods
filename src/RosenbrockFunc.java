afa9ee561090622c580d292409bd9936a3f43c7b
Natalia Senatorova <st068742@student.spbu.ru>
2019-12-09 20:04:20 +0300

    Update RosenbrockFunc.java

--- src/RosenbrockFunc.java
+++ src/RosenbrockFunc.java
@@ -1,22 +1,25 @@
 package ru.spbu.apmath.prog.methods;
 
-public class RosenbrockFunc implements Func{
+public class RosenbrockFunc implements Func {
     public double b;
     public double a;
-    RosenbrockFunc(double a, double b){
+
+    RosenbrockFunc(double a, double b) {
         this.a = a;
         this.b = b;
     }
-    public double val(Vector v){
+
+    public double val(Vector v) {
         double x = v.x;
         double y = v.y;
-        return Math.pow((a-x),2) + b*Math.pow((y - Math.pow(x,2)),2);
+        return Math.pow((a - x), 2) + b * Math.pow((y - Math.pow(x, 2)), 2);
     }
-    public Vector grad(Vector v){
+
+    public Vector grad(Vector v) {
         double x = v.x;
         double y = v.y;
-        double dx = (-2)*(a-x) - 4*b*(y - Math.pow(x,2))*x;
-        double dy = 2*b*(y-Math.pow(x,2));
+        double dx = (-2) * (a - x) - 4 * b * (y - Math.pow(x, 2)) * x;
+        double dy = 2 * b * (y - Math.pow(x, 2));
         return new Vector(dx, dy);
     }
 }
